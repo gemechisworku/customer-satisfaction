610ba422c8e8ee9ba6f187f29ddb729cd65a9b53
gemechisw <game.worku@gmail.com>
2025-08-08 12:45:24 +0300

    Experiment tracking with mlflow

--- steps/evaluation.py
+++ steps/evaluation.py
@@ -1,12 +1,15 @@
 import logging
-from zenml import step
 import pandas as pd
+import mlflow
+from zenml import step
+from zenml.client import Client
 from src.evaluation import MSE, R2, RMSE
 from sklearn.base import RegressorMixin
 from typing_extensions import Annotated
 from typing import Tuple
+experiment_tracker = Client().active_stack.experiment_tracker
 
-@step
+@step(experiment_tracker=experiment_tracker.name)
 def evaluate_model(
     model: RegressorMixin,
     X_test: pd.DataFrame,
@@ -19,12 +22,15 @@ def evaluate_model(
         prediction = model.predict(X_test)
         mse_evaluator = MSE()
         mse = mse_evaluator.evaluate_scores(y_test, prediction)
+        mlflow.log_metric("mse", mse)
 
         r2_evaluator = R2()
         r2 = r2_evaluator.evaluate_scores(y_test, prediction)
+        mlflow.log_metric("r2_score", r2)
 
         rmse_evaluator = RMSE()
         rmse = rmse_evaluator.evaluate_scores(y_test, prediction)
+        mlflow.log_metric("rmse", rmse)
 
         return r2, rmse
     except Exception as e:
--- steps/train_model.py
+++ steps/train_model.py
@@ -1,11 +1,13 @@
 import logging
 import pandas as pd
+import mlflow
 from zenml import step
+from zenml.client import Client
 from sklearn.base import RegressorMixin
 from src.model_dev import LinearRegressionModel
-# from .config import ModelNameConfig
+experiment_tracker = Client().active_stack.experiment_tracker
 
-@step
+@step(experiment_tracker=experiment_tracker.name)
 def train_model(
     X_train: pd.DataFrame,
     X_test: pd.DataFrame,
@@ -26,6 +28,7 @@ def train_model(
     model = None
     try:
         if model_name == "LinearRegression":
+            mlflow.sklearn.autolog()
             model = LinearRegressionModel()
             trained_model = model.train(X_train, y_train)
         else:
